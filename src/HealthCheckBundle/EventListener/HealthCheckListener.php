aaa12f48c105de847947a4785369524b0599780e
Ryan Lee <rol@bluetel.co.uk>
2019-12-30 14:48:58 +0000

    Automate unit tests

--- src/HealthCheckBundle/EventListener/HealthCheckListener.php
+++ src/HealthCheckBundle/EventListener/HealthCheckListener.php
@@ -1,4 +1,5 @@
-<?php
+<?php 
+
 namespace FT\HealthCheckBundle\EventListener;
 
 use Exception;
@@ -7,10 +8,9 @@ use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 
 /**
  * This event listner is used to force health checks to run before any other user defined code is executed.
- * This can be used in cases where other event listeners that might fail in the event of degraded site health stopping health checks from properly running
+ * This can be used in cases where other event listeners that might fail in the event of degraded site health stopping health checks from properly running 
  */
-class HealthCheckListener
-{
+class HealthCheckListener{
     const HEALTH_CHECK_ROUTE = '/__health';
 
     /**
@@ -18,8 +18,7 @@ class HealthCheckListener
      */
     protected $healthCheckController;
 
-    public function __construct(HealthCheckController $healthCheckController)
-    {
+    public function __construct(HealthCheckController $healthCheckController){
         $this->healthCheckController = $healthCheckController;
     }
 
@@ -27,13 +26,14 @@ class HealthCheckListener
     {
         
         // We only care about the master request in this instance
-        if (!$event->isMasterRequest()) {
+        if(!$event->isMasterRequest()){
             return;
         }
-        
+
         $request = $event->getRequest();
+
         //Do nothing when we are not specifically looking for the health check
-        if ($request->getPathInfo() !== self::HEALTH_CHECK_ROUTE) {
+        if($request->getPathInfo() !== self::HEALTH_CHECK_ROUTE){
             return;
         }
         
